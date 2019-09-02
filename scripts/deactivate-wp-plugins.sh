1c1f149e30f0887ddfe2eaa13a2af8ca0e690636
Philipp <philipp.hellmich@bertelsmann.de>
2019-09-02 12:51:17 +0200

    added deactivate plugin script

---
+++ scripts/deactivate-wp-plugins.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$WP_PLUGINS" = "" ]
+then
+   echo "please define WP_PLUGINS"
+   exit 1
+fi
+
+cd /app
+
+WP_PLUGINS_ACTIVE=$(wp plugin list | grep -v inactive | grep active | cut -f 1)
+
+for active_plugin in $WP_PLUGINS_ACTIVE
+do
+#  echo $active_plugin
+  for allowed_plugin in $WP_PLUGINS
+  do
+#    echo $check_plugin
+    if [ $active_plugin == $allowed_plugin ]
+    then
+            #echo "found, keeping $allowed_plugin"
+            continue
+    else
+            #echo "not found, deleting $active_plugin"
+            wp plugin deactivate $active_plugin --allow-root
+    fi
+  done
+done
